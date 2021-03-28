77627159802f10e43827913d427613135594b001
mdaffailhami <mdaffailhami@gmail.com>
2021-03-28 12:21:19 +0700

    Menambahkan backend

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @author Muhammad Daffa Ilhami
+ * @email mdaffailhami@gmail.com
+ * @create date 2021-03-28 - 12:18:25
+ * @modify date 2021-03-28 - 12:18:25
+ * @desc [Mulai menggunakan backend pada tanggal 28/03/2021]
+ */
+
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+const app = express();
